f53cc5b2e06579a05cbca9555932aa4046a5eb52
Karthik Rajan <karthikrcms@gmail.com>
2026-07-25 22:28:12 -0400

    Implement insert function

--- DSA_Assignment2/trees.c
+++ DSA_Assignment2/trees.c
@@ -1,1 +1,32 @@
+#include <stdlib.h>
 #include "trees.h"
+
+// Inserts a character into the binary search tree.
+Node* insert(Node* root, char value)
+{
+    // Create a new node when an empty position is found.
+    if (root == NULL)
+    {
+        Node* newNode = malloc(sizeof(Node));
+
+        newNode->data = value;
+        newNode->left = NULL;
+        newNode->right = NULL;
+
+        return newNode;
+    }
+
+    // Insert smaller values into the left subtree.
+    if (value < root->data)
+    {
+        root->left = insert(root->left, value);
+    }
+    // Insert larger values into the right subtree.
+    else if (value > root->data)
+    {
+        root->right = insert(root->right, value);
+    }
+
+    // Return the unchanged root pointer.
+    return root;
+}
--- DSA_Assignment2/trees.h
+++ DSA_Assignment2/trees.h
@@ -1,7 +1,6 @@
 #ifndef TREES_H
 #define TREES_H
 
-// Node used in the binary search tree.
 typedef struct Node
 {
     char data;
@@ -9,22 +8,16 @@ typedef struct Node
     struct Node* right;
 } Node;
 
-// Inserts a character into the tree.
 Node* insert(Node* root, char value);
 
-// Searches the tree for a character.
 Node* search(Node* root, char value);
 
-// Counts the number of nodes in the tree.
 int countNodes(Node* root);
 
-// Calculates the height of the tree.
 int treeHeight(Node* root);
 
-// Prints the tree in alphabetical order.
 void inorder(Node* root);
 
-// Frees all memory used by the tree.
 void freeTree(Node* root);
 
 #endif
