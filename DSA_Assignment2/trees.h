8cc0af7f52edbf40d7641abd6fadc32550ff2990
Karthik Rajan <karthikrcms@gmail.com>
2026-07-25 22:27:01 -0400

    Added function prototypes in trees.h

--- DSA_Assignment2/trees.h
+++ DSA_Assignment2/trees.h
@@ -9,4 +9,22 @@ typedef struct Node
     struct Node* right;
 } Node;
 
+// Inserts a character into the tree.
+Node* insert(Node* root, char value);
+
+// Searches the tree for a character.
+Node* search(Node* root, char value);
+
+// Counts the number of nodes in the tree.
+int countNodes(Node* root);
+
+// Calculates the height of the tree.
+int treeHeight(Node* root);
+
+// Prints the tree in alphabetical order.
+void inorder(Node* root);
+
+// Frees all memory used by the tree.
+void freeTree(Node* root);
+
 #endif
