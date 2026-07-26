b1084ba924c29fbe77105bf943576a9042de7ee1
Karthik Rajan <karthikrcms@gmail.com>
2026-07-25 22:35:17 -0400

    Add random character generation and tree creation

--- DSA_Assignment2/main.c
+++ DSA_Assignment2/main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "trees.h"
 
 int main(void)
 {
-    // Program entry point.
+    Node* root = NULL;
+
+    // Seed random number generator.
+    srand((unsigned int)time(NULL));
+
+    // Generate a random number between 11 and 20.
+    int numberOfChars = rand() % 10 + 11;
+
+    // Insert random characters into the tree.
+    for (int i = 0; i < numberOfChars; i++)
+    {
+        char randomChar = 'a' + rand() % 26;
+
+        root = insert(root, randomChar);
+    }
+
     return 0;
 }
