0bdc0e05bc080f419b21dddf8b361412539494ed
Karthik Rajan <karthikrcms@gmail.com>
2026-07-25 22:35:55 -0400

    Add output display and finalize binary search tree program

--- DSA_Assignment2/main.c
+++ DSA_Assignment2/main.c
@@ -10,16 +10,32 @@ int main(void)
     // Seed random number generator.
     srand((unsigned int)time(NULL));
 
-    // Generate a random number between 11 and 20.
+    // Generate random number of characters from 11 to 20.
     int numberOfChars = rand() % 10 + 11;
 
-    // Insert random characters into the tree.
+    printf("Characters inserted:\n");
+
+    // Insert random lowercase letters.
     for (int i = 0; i < numberOfChars; i++)
     {
         char randomChar = 'a' + rand() % 26;
 
+        printf("%c ", randomChar);
+
         root = insert(root, randomChar);
     }
+
+    printf("\n\nCharacters in alphabetical order:\n");
+
+    // Display BST using inorder traversal.
+    inorder(root);
+
+    printf("\n\nNumber of nodes: %d\n", countNodes(root));
+
+    printf("Tree height: %d\n", treeHeight(root));
+
+    // Release allocated memory.
+    freeTree(root);
 
     return 0;
 }
